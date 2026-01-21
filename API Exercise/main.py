497cce6f55d858d00b51fdbe0f6be10ccbed7d8b
Yann Djk <114170836+yanndjk@users.noreply.github.com>
2026-01-21 23:50:52 +0100

    switch main and branch content
    fixed mistake in commit and push

--- API Exercise/main.py
+++ API Exercise/main.py
@@ -7,7 +7,24 @@ app = FastAPI()
 
 @app.get("/{surface}")
 def read_item(surface: int):
-    return {"surface": surface}
+    '''
+    Get the surface area.
+
+    Args:
+        surface (int): The surface area to be returned.
+
+    Returns:
+        dict: A dictionary containing the surface area or an error message.
+    '''
+    try:
+        surface = int(surface)
+        
+        if surface < 0:
+            return {"error": "Surface area cannot be negative"}
+        return {"surface": surface}
+    
+    except ValueError:
+        return {"error": "Invalid input. Please provide a valid integer for surface area."}
 
 #test by pasting http://127.0.0.1:8000/50   in the browser after running the app with: uvicorn main:app --reload
 #you should see {"surface": 50}
